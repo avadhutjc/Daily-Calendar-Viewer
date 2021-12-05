966a4b5a83d8c2eea514be0097c8df19d9485f4b
Avadhutjc <Avadhutjc@gmail.com>
2021-12-05 16:35:24 +0530

    Calendar project done

--- app/src/main/java/com/ajc/dailycalendar/MainActivity.kt
+++ app/src/main/java/com/ajc/dailycalendar/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.ajc.dailycalendar
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.CalendarView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    var calendar: Calendar? = null
+    var calendarView: CalendarView? = null
+    protected override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        calendarView = findViewById(R.id.calendarView)
+
+        calendarView!!.setOnDateChangeListener { calendarView, i, i1, i2 ->
+            val msg = "Day: " + i2 + " Month : " + (i1 + 1) + " Year " + i
+            // Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show()
+
+            val toast = Toast.makeText(
+                applicationContext, msg, Toast.LENGTH_SHORT
+            )
+            toast.show()
+
+            val handler = Handler()
+            handler.postDelayed(Runnable { toast.cancel() }, 1000)
+        }
+    }
+
+    override fun onClick(view: View) {
+
     }
 }
